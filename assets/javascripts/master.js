70710eb565607e83369efb4987b08accc953e91e
julienmiclo <julien@billiotte.fr>
2019-05-16 18:17:48 +0200

    update(javascripts): update master.js, add select2

--- assets/javascripts/master.js
+++ assets/javascripts/master.js
@@ -1,6 +1,8 @@
-var buttonSide = document.querySelector('#buttonSide');
-var app = document.querySelector('#app');
+var buttonSide = document.querySelector('#buttonSide')
+var app = document.querySelector('#app')
 
 buttonSide.addEventListener('click', function() {
-    app.classList.toggle('side-is-open');
+    app.classList.toggle('side-is-open')
 });
+
+$('.select-select2').select2();
