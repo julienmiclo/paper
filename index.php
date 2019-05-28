6215a432c3f1172975a70eb05c3b8b00a3da4d3b
julienmiclo <julien@billiotte.fr>
2019-05-28 20:27:37 +0200

    feat(modules): feat calendar module

--- index.php
+++ index.php
@@ -54,29 +54,73 @@
 				<div class="container-page-content">
 
 					<div class="modules-row">
-						<div class="module-container">
-							<div class="module-content">
-								<h2>Paper time!</h2>
-								<p>This's a conceptual CRM PROJECT. It helps users to better manage the teams, projects & clients, generate reports and keep them up to date.</p>
+						<div class="modules-col">
+							<div class="module-container">
+								<div class="module-content">
+									<h2>Paper time!</h2>
+									<p>This's a conceptual CRM PROJECT. It helps users to better manage the teams, projects & clients, generate reports and keep them up to date.</p>
+								</div>
+							</div>
+							<div class="module-container">
+								<div class="module-head">
+									<h2 class="module-title">CRM - Management System</h2>
+									<div class="module-actions">
+										<a href="#" class="item"><i class="material-icons">cloud_download</i></a>
+										<button class="item"><i class="material-icons">more_horiz</i></button>
+									</div>
+								</div>
+								<div class="module-content">
+									<p>This's a conceptual CRM PROJECT. It helps users to better manage the teams, projects & clients, generate reports and keep them up to date.</p>
+									<button class="button">S'inscrire avec une adresse e-mail</button>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="modules-row">
+
 						<div class="module-container">
 							<div class="module-head">
-								<h2 class="module-title">CRM - Management System</h2>
+								<h2 class="module-title">Calendar</h2>
 								<div class="module-actions">
-									<a href="#" class="item"><i class="material-icons">cloud_download</i></a>
 									<button class="item"><i class="material-icons">more_horiz</i></button>
 								</div>
 							</div>
 							<div class="module-content">
-								<p>This's a conceptual CRM PROJECT. It helps users to better manage the teams, projects & clients, generate reports and keep them up to date.</p>
-								<button class="button">S'inscrire avec une adresse e-mail</button>
+								<?php
+								$date = new \DateTime();
+								$date->setTime( 0, 0, 0 );
+								$start = (clone $date)->modify('first day of this month');
+								$start->setTime( 0, 0, 0 );
+								$end = (clone $start)->modify('+1 month -1 day');
+								$end->setTime( 0, 0, 0 );
+								$week = intval((clone $start)->diff($end)->days / 7) + 1;
+								$lastMonday = $start->modify('last monday');
+								?>
+								<div class="wrap-calendar">
+									<table class="calendar">
+										<tr class="head">
+											<?php for ($i=0; $i < 7; $i++):?>
+												<th class="day"><?php echo (clone $start)->modify('+'.$i.' days')->format('D'); ?></th>
+											<?php endfor;?>
+										</tr>
+										<?php for ($w=0; $w < $week; $w++):?>
+											<tr class="week">
+												<?php for ($i=0; $i < 7; $i++):?>
+													<?php
+														$day = (clone $lastMonday)->modify('+'.($i+($w*7)).' days');
+														$diff = $date->diff($day)->days;
+													?>
+													<td class="day <?php echo ($diff === 0)?'today':'';?> <?php echo ($day->format('N') >= 6)?'weekend':'' ?>">
+														<?php echo ($day->format('m') === $date->format('m'))?$day->format('d'):''; ?>
+													</td>
+												<?php endfor;?>
+											</tr>
+										<?php endfor;?>
+									</table>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div class="modules-row">
+
+					<div class="modules-row m1">
 						<div class="module-container">
 							<div class="module-head">
 								<h2 class="module-title">Agenda</h2>
@@ -173,7 +217,7 @@
 						</div>
 					</div>
 
-					<div class="modules-row">
+					<div class="modules-row m1">
 						<div class="module-container">
 							<div class="module-head">
 								<h2 class="module-title">Users</h2>
@@ -243,7 +287,9 @@
 						</div>
 					</div>
 
-					<div class="modules-row">
+
+
+					<div class="modules-row m1">
 						<div class="module-container">
 							<div class="module-head">
 								<h2 class="module-title">Projects</h2>
@@ -282,7 +328,7 @@
 						</div>
 					</div>
 
-					<div class="modules-row">
+					<div class="modules-row m1">
 						<div class="module-container">
 							<div class="module-head">
 								<h2 class="module-title">New project</h2>
