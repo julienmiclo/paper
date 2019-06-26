0b182f730673bbf6ebd627e49feba498a909a7ae
julienmiclo <julienmiclo@gmail.com>
2019-06-26 09:07:23 +0200

    feat(pages): add pages

---
+++ pages/errors.php
@@ -0,0 +1,8 @@
+<div class="container-errors">
+  <div class="error-content">
+    <h2><?php echo $_GET['errors']; ?></h2>
+    <h3>Something went wrong!</h3>
+    <p>There was a problem on our end. Please try again later.</p>
+    <a href="#" class="button"><i class="material-icons start">arrow_back</i>Go Back</a>
+  </div>
+</div>
